35a8fa9caa7d265774bc49d7ef52ae7fd6f08449
Baha <88669412+JegBaha@users.noreply.github.com>
2025-02-17 12:32:43 +0300

    22.

--- YazilimProje.Tests/UnitTest1.cs
+++ YazilimProje.Tests/UnitTest1.cs
@@ -60,7 +60,7 @@ namespace YazilimProje.Tests
             // Act
             DataTable customers = dbHelper.GetCustomers();
 
-            // Asserta
+            // Assertaa
             Assert.AreEqual(2, customers.Rows.Count);
 
             DataRow firstRow = customers.Rows[0];
