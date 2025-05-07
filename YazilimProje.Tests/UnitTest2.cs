1a7324d0c42a267b87a8a28b4fdf7265b67351b8
Baha <88669412+JegBaha@users.noreply.github.com>
2025-05-07 18:55:50 +0300

    Deneme123

---
+++ YazilimProje.Tests/UnitTest2.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace YazilimProje.Tests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Fail("Deliberate failure to demonstrate test failure in Jenkins.");
+        }
+    }
+}
